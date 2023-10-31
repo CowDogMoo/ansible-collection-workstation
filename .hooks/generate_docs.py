626d673ab34f63fe037e57a49865efdb179581e7
Jayson Grace <jayson.e.grace@gmail.com>
2023-10-30 23:25:09 -0600

    Refactored `generate_docs.py` to improve YAML processing and table generation.
    **Changed:**
    
    - Simplified `process_yaml` function to utilize a while loop for better handling
      of multi-line values, ensuring accurate value and description extraction.
    - Removed `extract_descriptions` function as it is no longer needed.
    - Modified `generate_row` function to accurately format different types of
      values including lists and Ansible expressions.
    - Improved `generate_table` function to create Markdown tables with consistent
      formatting and proper handling of different value types.
    - Updated tables in `roles/asdf/README.md` to reflect the new formatting and
      value extraction logic, ensuring consistency and readability.

--- .hooks/generate_docs.py
+++ .hooks/generate_docs.py
@@ -1,81 +1,54 @@
 import os
 import re
+import yaml
 
 def process_yaml(var_path):
-    """Process a YAML file to extract variables, their values, and descriptions.
-
-    Args:
-        var_path (str): Path to the YAML file.
-
-    Returns:
-        dict: A dictionary where keys are variable names and values are tuples
-              containing the variable value and description.
-    """
+    var_dict = {}
+    description = ""
     with open(var_path, 'r') as file:
         lines = file.readlines()
 
-    var_dict = {}
-    description = None
-    var_name = None
-    var_value = None
-    is_list = False
-    for line in lines:
-        stripped_line = line.strip()
-        if stripped_line.startswith('#'):
-            if var_name:  # Save previous variable if a description is found
-                var_dict[var_name] = (var_value, description)
-                var_name = None  # reset var_name for next variable
-            description = stripped_line.lstrip('# ').strip()
-        elif ':' in stripped_line:
-            var_name, var_value = stripped_line.split(':', 1)
-            var_name = var_name.strip()
+    i = 0
+    while i < len(lines):
+        line = lines[i].strip()
+        if line.startswith('#'):
+            description = line.lstrip('# ').strip()
+        elif ':' in line:
+            var_name, var_value = line.split(':', 1)
             var_value = var_value.strip()
-            is_list = False
-        elif stripped_line.startswith('- ') and var_name:
-            item = stripped_line.lstrip('- ').strip()
-            if not is_list:
-                var_value = [item]
-                is_list = True
+            # Accumulate lines for this value until we reach a new key or the end of the file
+            while (i + 1 < len(lines) and
+                   not (lines[i + 1].strip().startswith('#') or ':' in lines[i + 1])):
+                i += 1
+                var_value += '\n' + lines[i].strip()  # Join lines with newline
+            # Now process the complete value string
+            if var_value.startswith('{{') and var_value.endswith('}}'):
+                pass  # Keep Ansible expressions as-is
             else:
-                var_value.append(item)
-
-    if var_name:  # Save last variable
-        var_dict[var_name] = (var_value, description)
+                var_value = yaml.safe_load(var_value)
+            var_dict[var_name.strip()] = (var_value, description)
+            description = ""  # Reset description for next variable
+        i += 1  # Move on to the next line
 
     return var_dict
 
 
 
-def extract_descriptions(readme_path):
-    """Extract variable descriptions from a README file.
-
-    Args:
-        readme_path (str): Path to the README file.
-
-    Returns:
-        dict: A dictionary where keys are variable names and values are descriptions.
-    """
-    with open(readme_path, 'r') as file:
-        content = file.read()
-
-    descriptions = {}
-    for match in re.finditer(r"\|\s*`([^`]+)`\s*\|\s*`([^`]+)`\s*\|\s*([^|]+)\s*\|", content):
-        var_name, default_value, description = match.groups()
-        descriptions[var_name] = description.strip()
-
-    return descriptions
+def generate_row(var, value_desc_tuple):
+    value, description = value_desc_tuple
+    if value is None:
+        value_str = '`None`'
+    elif isinstance(value, str) and (value.startswith('{{') and value.endswith('}}')):
+        value_str = f'`{value}`'
+    elif isinstance(value, list):
+        # If value is a list, convert the list to a string and enclose it in backticks
+        value_str = f'`{", ".join(value)}`'
+    else:
+        # For other types, convert them to string and enclose it in backticks
+        value_str = f'`{str(value)}`'
+    return f"| `{var}` | {value_str} | {description} |"
 
 def generate_table(role_path):
-    """Generate a markdown table of variables, their default values, and descriptions
-       from the YAML files in the specified role path.
-
-    Args:
-        role_path (str): Path to the role directory containing the vars subdirectory
-                         and YAML files.
-
-    Returns:
-        str: A string representing the markdown table.
-    """
     os_vars = {}
     general_vars = {}
 
@@ -84,35 +57,25 @@ def generate_table(role_path):
         var_path = os.path.join(role_path, 'vars', var_file)
         if os.path.exists(var_path):
             vars_dict = process_yaml(var_path)
-
             if os_family == 'main':
                 general_vars.update(vars_dict)
             else:
                 os_family_cap = os_family.capitalize()
                 os_vars.setdefault(os_family_cap, {}).update(vars_dict)
 
-    # Generate table for general variables
     general_table_header = "| Variable | Default Value | Description |\n| --- | --- | --- |\n"
-    general_table_rows = "".join([f"| `{var}` | `{str(value).replace('|', '\\|')}` | {desc} |\n" for var, (value, desc) in general_vars.items()])
-
+    general_table_rows = "\n".join([generate_row(var, value_desc_tuple) for var, value_desc_tuple in general_vars.items()])
     general_table = general_table_header + general_table_rows
 
-    # Generate tables for OS-specific variables
     os_tables = []
     for os_family, var_dict in os_vars.items():
         table_header = f"| Variable | Default Value ({os_family}) | Description |\n| --- | --- | --- |\n"
-        table_rows = "".join([f"| `{var}` | `{str(value).replace('|', '\\|')}` | {desc} |\n" for var, (value, desc) in var_dict.items()])
+        table_rows = "\n".join([generate_row(var, value_desc_tuple) for var, value_desc_tuple in var_dict.items()])
         os_tables.append(table_header + table_rows)
 
     return general_table + "\n" + "\n".join(os_tables)
 
 def main():
-    """Main function to generate markdown tables for each role and update README files.
-
-    This function does not take any arguments or return any values. It reads from the
-    'roles' directory, generates markdown tables for each role, and updates the README
-    files with the new tables.
-    """
     roles_path = "roles"
     for role_name in os.listdir(roles_path):
         role_path = os.path.join(roles_path, role_name)
@@ -124,9 +87,9 @@ def main():
                     readme_content = file.read()
                 if "<!--- end vars table -->" in readme_content:
                     new_readme_content = readme_content.split("<!--- vars table -->")[0] + \
-                                         "<!--- vars table -->\n" + table + \
-                                         "<!--- end vars table -->" + \
-                                         readme_content.split("<!--- end vars table -->")[1]
+                         "<!--- vars table -->\n" + table + "\n" + \
+                         "<!--- end vars table -->" + \
+                         readme_content.split("<!--- end vars table -->")[1]
                     with open(readme_path, 'w') as file:
                         file.write(new_readme_content)
                 else:
