89c385013366a338a23850576257710b2df35094
Jayson Grace <jayson.e.grace@gmail.com>
2023-11-29 13:15:19 -0700

    Refactored asdf role for improved idempotence and user-specific configurations
    - Simplified asdf_users variable usage in `defaults/main.yml`, ensuring consistent
      user references across all tasks and templates.
    - Enhanced `setup_asdf_env.sh` script for robust user validation and ASDF_PATH
      checking, preventing potential runtime errors.
    - Refined `converge.yml` in `molecule/default` for streamlined variable inclusion
      and better role integration within the molecule test setup.
    - Updated `verify.yml` in `molecule/default` to assert the new location of
      `setup_asdf_env.sh` and streamlined checks for ASDF accessibility and proper
      plugin installation.
    - Modified tasks in `main.yml` and `package_individual_setup.yml` to incorporate
      checks for existing plugins and versions, ensuring tasks only make changes when
      necessary (idempotence).
    - Removed redundant looping over `asdf_packages` in `package_setup.yml`, replacing
      it with a loop over `asdf_users` for consistency.
    - Updated `tool-versions.j2` template to iterate over users and their respective
      plugins for a more dynamic and user-specific tool version setup.

--- roles/asdf/files/setup_asdf_env.sh
+++ roles/asdf/files/setup_asdf_env.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-set -e
+set -ex
+
+if [ -z "$1" ]; then
+    echo "No username provided"
+    exit 1
+fi
 
 export ASDF_PATH="/home/$1/.asdf"
 export PATH="$ASDF_PATH/bin:$ASDF_PATH/shims:$PATH"
-. "$ASDF_PATH/asdf.sh"
+
+# Check if ASDF_PATH is correctly set
+if [[ ! -d "$ASDF_PATH" ]]; then
+    echo "ASDF_PATH '$ASDF_PATH' does not exist"
+    exit 1
+fi
