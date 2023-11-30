ea596ad5adaab83c8329f180d77a9920fbdbe8ab
Jayson Grace <jayson.e.grace@gmail.com>
2023-11-30 02:00:55 -0700

    Refactor ASDF role for improved compatibility and functionality
    **Changed:**
    
    - Updated shell syntax in `asdf/defaults/main.yml` for ASDF path and plugin setup,
      transitioning from double-quoted strings to single quotes for consistency.
    - Modified `setup_asdf_env.sh` script for consistent indentation and improved
      error handling messages.
    - Simplified plugin verification in `asdf/molecule/default/verify.yml` by
      replacing command module with shell module, incorporating `set -o pipefail`
      for more reliable error detection.
    - Standardized file mode syntax in `asdf/tasks/main.yml` for `.tool-versions`
      file and `setup_asdf_env.sh` script to double-quoted strings.
    - Inserted a TODO comment in `asdf/tasks/main.yml` to flag an area needing
      future enhancements in plugin and version gathering.
    - Removed an unnecessary environment variable setting in `asdf/tasks/main.yml`.
    
    **Fixed:**
    
    - Adjusted various lines for whitespace and formatting consistency across
      multiple files in the ASDF role for better readability and maintainability.

--- roles/asdf/files/setup_asdf_env.sh
+++ roles/asdf/files/setup_asdf_env.sh
@@ -2,8 +2,8 @@
 set -ex
 
 if [ -z "$1" ]; then
-    echo "No username provided"
-    exit 1
+	echo "No username provided"
+	exit 1
 fi
 
 export ASDF_PATH="/home/$1/.asdf"
@@ -11,6 +11,6 @@ export PATH="$ASDF_PATH/bin:$ASDF_PATH/shims:$PATH"
 
 # Check if ASDF_PATH is correctly set
 if [[ ! -d "$ASDF_PATH" ]]; then
-    echo "ASDF_PATH '$ASDF_PATH' does not exist"
-    exit 1
+	echo "ASDF_PATH '$ASDF_PATH' does not exist"
+	exit 1
 fi
