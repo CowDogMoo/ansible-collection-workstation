c2867f855f076fb0b2f5a288d9eed4d18f9ce18f
Jayson Grace <jayson.e.grace@gmail.com>
2023-12-07 16:10:51 -0700

    Refactor asdf role to fix path issues and improve shell profile updates
    - Fixed incorrect ASDF_PATH in setup_asdf_env script to ensure correct home directory.
    - Updated main.yml to include a new task file for updating shell profiles.
    - Modified package_individual_setup.yml to pass the correct home directory.
    - Introduced new file update_shell_profiles.yml for handling shell profile updates.
    
    This change resolves the path duplication issue in ASDF_PATH and streamlines
    the process of updating user shell profiles, enhancing role reliability.

--- roles/asdf/files/setup_asdf_env.sh
+++ roles/asdf/files/setup_asdf_env.sh
@@ -6,10 +6,14 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-export ASDF_PATH="/home/$1/.asdf"
+if [ -z "$2" ]; then
+	echo "No home directory provided"
+	exit 1
+fi
+
+export ASDF_PATH="$2/$1/.asdf"
 export PATH="$ASDF_PATH/bin:$ASDF_PATH/shims:$PATH"
 
-# Check if ASDF_PATH is correctly set
 if [[ ! -d "$ASDF_PATH" ]]; then
 	echo "ASDF_PATH '$ASDF_PATH' does not exist"
 	exit 1
