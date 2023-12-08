9ab970b9a6048410f7e0db2698c4d972f1f87da6
Jayson Grace <jayson.e.grace@gmail.com>
2023-12-08 01:49:39 -0700

    Refactor asdf role to simplify setup_asdf_env and adjust tasks
    **Changed:**
    - Simplified setup_asdf_env.sh script by removing extra parameters and
      improving error handling.
    - Modified the `home_path` variable in defaults/main.yml to dynamically set
      the default home path based on OS family.
    - Updated tasks in main.yml and package_individual_setup.yml to use the new
      `home_path` variable, streamlining the setup process.
    - Enhanced error messages and streamlined variable usage in shell commands.
    - Added debug tasks for `home_path` and `user_plugin` variables to aid in
      troubleshooting and verification.
    - Unified the approach to setting `ASDF_PATH` across different tasks for
      consistency.
    - Corrected conditional checks in the shell script to properly handle scope
      settings ('global' or 'local') for asdf plugin versions.

--- roles/asdf/files/setup_asdf_env.sh
+++ roles/asdf/files/setup_asdf_env.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-set -ex
+echo "Running setup_asdf_env.sh with user home path: $1"
 
 if [ -z "$1" ]; then
-	echo "No username provided"
+	echo "No user home path provided" >&2
 	exit 1
 fi
 
-if [ -z "$2" ]; then
-	echo "No home directory provided"
-	exit 1
-fi
-
-export ASDF_PATH="$2/$1/.asdf"
+export ASDF_PATH="$1/.asdf"
 export PATH="$ASDF_PATH/bin:$ASDF_PATH/shims:$PATH"
 
+echo "Set ASDF_PATH to: $ASDF_PATH"
+
 if [[ ! -d "$ASDF_PATH" ]]; then
-	echo "ASDF_PATH '$ASDF_PATH' does not exist"
+	echo "ASDF_PATH '$ASDF_PATH' does not exist" >&2
 	exit 1
 fi
